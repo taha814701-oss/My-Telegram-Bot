44805a947a0a1de6b6ce36d361db7338174f74e9
taha814701-oss <taha814701@gmail.com>
2026-07-18 23:23:47 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
+import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-# توکن رباتت رو اینجا بذار
-TOKEN ="1713259160:AAGxXKBTX9j7Pe31cBMOAmzcLsp70HMwAF8"
+TOKEN = "1713259160:AAGxXKBTX9j7Pe31cBMOAmzcLsp70HMwAF8"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! 🤖 من رباتت هستم.")
@@ -10,9 +10,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"گفتی: {update.message.text}")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    
+    print("ربات روشن شد...")
+    await app.run_polling()
 
-print("ربات روشن شد...")
-app.run_polling()
+if __name__ == "__main__":
+    asyncio.run(main())
